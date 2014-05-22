bb7bde34d9ada9e3a1ba5d481b5330067e9683c4
Coeuvre <coeuvre@gmail.com>
2014-05-22 16:19:42 +0800

    Added `test_sub_image_app`

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate piston;
 use piston::*;
 
 mod test_image_app;
+mod test_sub_image_app;
 mod test_mouse_move_event_app;
 
 type GameWindowBackEnd = GameWindowSDL2;
@@ -18,14 +19,15 @@ fn main() {
             [300, 300],
             false,
             true,
-            [1.0, 1.0, 1.0, 1.0]
+            [0.0, 0.0, 0.0, 0.0]
         )
     );
 
     let mut asset_store = AssetStore::from_folder("assets");
 
     //let mut app = test_mouse_move_event_app::App::new();
-    let mut app = test_image_app::App::new();
+    //let mut app = test_image_app::App::new();
+    let mut app = test_sub_image_app::App::new();
 
     app.run(&mut game_window, &mut asset_store);
 }
--- src/test_sub_image_app.rs
+++ src/test_sub_image_app.rs
@@ -16,11 +16,14 @@ impl App {
 
 impl Game for App {
     fn load(&mut self, asset_store: &mut AssetStore) {
-        self.image = Some(asset_store.load_image("char01.png"));
+        self.image = Some(asset_store.load_image("digits.png"));
     }
 
     fn render(&self, c: &Context, gl: &mut Gl) {
-        c.view().trans_local(100.0, 50.0).image(self.image.unwrap()).draw(gl);
+        let mut image = self.image.unwrap();
+        image.source_rect[2] = 20;
+        image.source_rect[3] = 26;
+        c.view().image(image).draw(gl);
     }
 }
 
